75b60258f58f9beb9fa0f155486161165090f133
Taneja, Kapil <Kapil.Taneja@pearson.com>
2017-10-26 17:03:05 +0530

    Adding compression as well

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var path = require("path")
 var app = express();
+var compress = require("compression")
 var multer = require("multer");
 var xlJson = require("./index.js")
 var fs = require("fs");
 const port = process.env.PORT || 3000;
 app.set('port', port);
 app.listen(port);
+app.use(compress());
 console.log("listening on port 3000");
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, './index.html'));
